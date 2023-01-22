aca4001a0c181d1f083a0321c2f36a9220d0159a
AfraidMonster <henrygfraser@icloud.com>
2023-01-22 14:52:30 +0000

    Initial commit

---
+++ src/main/java/afraidmonster/moredoors/CustomDoorBlock.java
@@ -0,0 +1,19 @@
+package afraidmonster.moredoors;
+
+import net.minecraft.block.DoorBlock;
+import net.minecraft.sound.SoundEvent;
+
+
+public class CustomDoorBlock extends DoorBlock {
+
+    public CustomDoorBlock(Settings settings, SoundEvent closeSound, SoundEvent openSound) {
+        super(settings.nonOpaque(), closeSound, openSound);
+        
+        //TODO Auto-generated constructor stub
+    }
+
+    
+
+    
+
+}
---
+++ src/main/java/afraidmonster/moredoors/Mixin/WoodenDoorMixin.java
@@ -0,0 +1,36 @@
+package afraidmonster.moredoors.Mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.DoorBlock;
+import net.minecraft.registry.tag.BlockTags;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DoorBlock.class)
+
+public class WoodenDoorMixin {
+
+
+
+    @Inject(at = @At(value = "HEAD"), method = "isWoodenDoor(Lnet/minecraft/block/BlockState;)Z", cancellable = true)
+    private static void isWoodenDoor(BlockState blockState, CallbackInfoReturnable info) {
+        if (blockState.getBlock() instanceof DoorBlock && (blockState.isIn(BlockTags.WOODEN_DOORS))) {
+            info.setReturnValue(true);
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
+    
+
+ 
