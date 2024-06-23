876c745f20e3b906b93abba1184cfb8e571c7ec5
AfraidMonster <henrygfraser@icloud.com>
2024-06-23 20:06:00 +0100

    v1.6

---
+++ src/main/java/afraidmonster/moredoors/CustomTintedTrapdoorBlock.java
@@ -0,0 +1,23 @@
+package afraidmonster.moredoors;
+
+import net.minecraft.block.BlockSetType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.TrapdoorBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+
+public class CustomTintedTrapdoorBlock extends TrapdoorBlock {
+
+    public CustomTintedTrapdoorBlock(Settings settings, BlockSetType blockSetType) {super(settings.nonOpaque(), blockSetType);}
+
+    public boolean isTransparent(BlockState state, BlockView world, BlockPos pos) {
+        return false;
+    }
+
+    public int getOpacity(BlockState state, BlockView world, BlockPos pos) {
+        return world.getMaxLightLevel();
+    }
+
+}
+
+   
